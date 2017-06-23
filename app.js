71c0e116a09f1c1dc688bf26b9189c26cf190277
Christina Cornell <cdhorn515@gmail.com>
2017-06-23 17:43:28 -0400

    added game landing page and login redirects to it

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ var path = require('path');
 var session = require('express-session');
 var bodyParser = require('body-parser');
 var parseurl = require('parseurl');
+var expressValidator = require('express-validator');
 var app = express();
 
 app.engine('mustache', mustacheExpress());
@@ -11,13 +12,33 @@ app.set('view engine', 'mustache');
 app.set('views', path.join(__dirname, 'views'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}));
+app.use(expressValidator());
 app.use('/static', express.static(path.join(__dirname, 'public')));
+
 app.use(session({
   secret: 'unicorn cats',
   resave: false,
   saveUninitialized: true
 }));
-var context = {};
+
+var context = [
+    {'name': 'Sami'}
+];
+
+// check to see if we have a session for name
+app.use(function(req, res, next) {
+  var pathname = parseurl(req).pathname;
+
+  if (req.body.name) {
+      console.log(req.body.name);
+    res.redirect('/mysteryword');
+  } else {
+    // res.redirect('/');
+    console.log('enter a name please');
+
+    next();
+  }
+});
 
 app.get('/', function(req, res){
   res.render('index');
@@ -27,9 +48,20 @@ app.get('/login', function(req, res){
   res.render('login');
 });
 
-app.post('/', function(req, res){
-  console.log('username');
-  res.render('index');
+app.get('/mysteryword', function(req,res){
+  res.render('mysteryword');
+});
+
+app.post('/login', function(req, res){
+  var username = req.body.name;
+  var session = req.session;
+  console.log(username);
+  console.log(session);
+  if (req.body.name){
+  res.redirect('/mysteryword');
+} else {
+  res.redirect('/login');
+}
 });
 
 app.listen(3000, function(){
