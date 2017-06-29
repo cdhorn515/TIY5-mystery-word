01da0f389633cadb455ca3177ab005def45aa65d
Christina Cornell <cdhorn515@gmail.com>
2017-06-28 21:07:46 -0400

    all error validation except checking for empty working

--- controllers/mysteryWord.js
+++ controllers/mysteryWord.js
@@ -21,69 +21,75 @@ module.exports = {
       guessedLetters: req.session.guessedLetters
     };
     req.session.guess = req.body.guess;
-
-    if (req.body.guess) {
-      console.log('87 ', typeof req.body.guess);
-      console.log('85 ', req.session.guessesLeft);
-      req.checkBody('guess', 'Hey what kind of stunt are you trying to pull? Please just enter one letter').isLength({
-        max: 1
-      });
-    }
-    var errors = req.validationErrors();
-    if (!errors) {
-      req.session.guess = req.body.guess;
-      context.guess = req.session.guess;
-      // console.log('here', req.session.guessedLetters.length);
-      //check for duplicate letters
-      if (req.session.guessedLetters.length > 0) {
-        for (var i = 0; i < req.session.guessedLetters.length; i++) {
-          if (req.session.guessedLetters.includes(req.session.guess)) {
-            context.duplicateLetterMessage = "Hey, stop guessing me!!!!!";
-            res.render('mysteryWord', context);
-            return;
-          }
-        }
-      }
-
-      //push guessed letter into array to display on screen
-      req.session.guessedLetters.push(req.body.guess);
-      context.guessedLetters = req.session.guessedLetters;
-      // req.session.guessesLeft--;
-      // }
-      // }
-      //loop through and check guessed letter against letters in word
-      req.session.lettersCorrect = 0;
-      for (var i = 0; i < req.session.word.length; i++) {
-        if (req.session.guess === req.session.secretWord[i]) {
-          //this is working, displays letters in place of blanks
-          req.session.wordBlanks[i] = req.session.guess;
-          req.session.lettersCorrect++;
-          //remove one from the length of the word, when it's 0 the word is guessed
-          //this is working, lengthOfWord goes to 0 as correct guesses are made
-          req.session.lengthOfWord--;
-          if (req.session.lengthOfWord === 0) {
-            context.gameOver = 'Congratulations, you guessed the secret word!';
-            res.render('mysteryWord', context);
-            return;
-          }
-        }
-      }
-      if (req.session.lettersCorrect === 0) {
-        req.session.guessesLeft--;
-        if (req.session.guessesLeft === 0) {
-          console.log('line 137 no guesses left ' + req.session.guessesLeft);
-          context.guessesLeft = 0;
-          context.gameOver = "Better luck next time!"
-          res.render('mysteryWord', context);
-          return;
-        }
-      }
-      context.guessesLeft = req.session.guessesLeft;
-
+    if (!req.body.guess) {
+      context.error = "If you want to play the game, you have to enter a letter";
+      res.render('mysteryWord', context);
+      return;
     } else {
-      context.errors = errors;
-    }
+      if (req.body.guess) {
+        console.log('85 ', req.session.guessesLeft);
+        req.checkBody('guess', 'Hey what kind of stunt are you trying to pull? One letter at a time please.').isLength({
+          min: 1,
+          max: 1
+        });
+        req.checkBody('guess', 'I don\'t know where you\'re from, but around here we only use letters in our words.').isAlpha();
+      }
+      var errors = req.validationErrors();
+      if (!errors) {
+        req.session.guess = req.body.guess;
+        context.guess = req.session.guess;
+        // console.log('here', req.session.guessedLetters.length);
+        //check for duplicate letters
+        if (req.session.guessedLetters.length > 0) {
+          for (var i = 0; i < req.session.guessedLetters.length; i++) {
+            if (req.session.guessedLetters.includes(req.session.guess)) {
+              context.duplicateLetterMessage = "Hey, stop guessing me!!!!!";
+              res.render('mysteryWord', context);
+              return;
+            }
+          }
+        }
 
+        //push guessed letter into array to display on screen
+        req.session.guessedLetters.push(req.body.guess);
+        context.guessedLetters = req.session.guessedLetters;
+        // req.session.guessesLeft--;
+        // }
+        // }
+        //loop through and check guessed letter against letters in word
+        req.session.lettersCorrect = 0;
+        for (var i = 0; i < req.session.word.length; i++) {
+          if (req.session.guess === req.session.secretWord[i]) {
+            //this is working, displays letters in place of blanks
+            req.session.wordBlanks[i] = req.session.guess;
+            req.session.lettersCorrect++;
+            //remove one from the length of the word, when it's 0 the word is guessed
+            //this is working, lengthOfWord goes to 0 as correct guesses are made if 0 then displays congrats message
+            req.session.lengthOfWord--;
+            if (req.session.lengthOfWord === 0) {
+              context.gameOver = 'Congratulations, you guessed the secret word!';
+              res.render('mysteryWord', context);
+              return;
+            }
+          }
+        }
+        if (req.session.lettersCorrect === 0) {
+          req.session.guessesLeft--;
+          //this is working, but need to fix the word it displays
+          if (req.session.guessesLeft === 0) {
+            console.log('line 137 no guesses left ' + req.session.guessesLeft);
+            context.guessesLeft = 0;
+            context.gameOver = "Better luck next time!";
+            res.render('mysteryWord', context);
+            return;
+          }
+        }
+        context.guessesLeft = req.session.guessesLeft;
+
+      } else {
+        context.errors = errors;
+      }
+    }
     res.render('mysteryWord', context);
 
     // context.guessesLeft--;
