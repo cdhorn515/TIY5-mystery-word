91fe83d9497459ab9b67432bbee8932117f0c2ec
Christina Cornell <cdhorn515@gmail.com>
2017-06-29 16:02:09 -0400

    doesn't display guesses left when game is over

--- controllers/mysteryWord.js
+++ controllers/mysteryWord.js
@@ -20,7 +20,7 @@ module.exports = {
       lengthOfWord: req.session.lengthOfWord,
       guessedLetters: req.session.guessedLetters
     };
-    req.session.guess = req.body.guess;
+
     if (req.body.guess === '') {
       context.error = "If you want to play the game, you have to enter a letter";
       res.render('mysteryWord', context);
@@ -35,7 +35,8 @@ module.exports = {
       }
       var errors = req.validationErrors();
       if (!errors) {
-        req.session.guess = req.body.guess;
+        req.session.guess = req.body.guess.toLowerCase();
+        console.log('39', req.session.guess);
         context.guess = req.session.guess;
         //check for duplicate letters
         if (req.session.guessedLetters.length > 0) {
@@ -47,19 +48,19 @@ module.exports = {
             }
           }
         }
-        //push guessed letter into array to display on screen
-        req.session.guessedLetters.push(req.body.guess);
+        //push guessed letter into array to display on page
+        req.session.guessedLetters.push(req.session.guess);
         context.guessedLetters = req.session.guessedLetters;
-        //loop through and check guessed letter against letters in word
+        //loop thru and check guessed letter against letters in word
         req.session.lettersCorrect = 0;
         for (var i = 0; i < req.session.word.length; i++) {
           if (req.session.guess === req.session.secretWord[i]) {
             //this is working, displays letters in place of blanks
             req.session.wordBlanks[i] = req.session.guess;
             req.session.lettersCorrect++;
-            //remove one from the length of the word, when it's 0 the word is guessed
-            //this is working, lengthOfWord goes to 0 as correct guesses are made if 0 then displays congrats message
+            //lengthOfWord decreases as correct guesses are made if 0 then displays congrats message
             req.session.lengthOfWord--;
+            context.lengthOfWord = req.session.lengthOfWord;
             if (req.session.lengthOfWord === 0) {
               context.gameOver = 'Congratulations, you guessed the secret word!';
               res.render('mysteryWord', context);
@@ -69,7 +70,6 @@ module.exports = {
         }
         if (req.session.lettersCorrect === 0) {
           req.session.guessesLeft--;
-          //this is working, but need to fix the word it displays
           if (req.session.guessesLeft === 0) {
             context.guessesLeft = 0;
             context.gameOver = "Better luck next time!";
@@ -78,7 +78,6 @@ module.exports = {
           }
         }
         context.guessesLeft = req.session.guessesLeft;
-
       } else {
         context.errors = errors;
       }
